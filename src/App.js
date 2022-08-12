61a27fec141413b5af73f52cb816679dd5d7b109
dhruv <dhruvgoel343@gmail.com>
2022-08-12 19:25:47 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div>   <Navs /></div>
+      
+      <Routes>
+        <Route exact={true} path="/" element={<Home/>}></Route>
+        <Route path="/starred" element={<Starred/>}></Route>
+       
+      </Routes>
+    </>
+    
   );
 }
 
---
+++ src/pages/Starred.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Starred = () => {
+  return (
+    <div>this is Starred</div>
+  )
+}
+
+export default Starred
